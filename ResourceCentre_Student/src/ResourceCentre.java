21ef154d2f56dba0bb383c8140752caea0c0fa1d
Lai Suan <Lai Suan@10.175.3.89>
2023-07-21 11:36:03 +0800

    Refactored methods

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -27,9 +27,7 @@ public class ResourceCentre {
             } else if (option == 2) {
                 // Add a new item
                 ResourceCentre.setHeader("ADD");
-                ResourceCentre.setHeader("ITEM TYPES");
-                System.out.println("1. Camcorder");
-                System.out.println("2. Chromebook");
+                itemTypeMenu();
 
                 int itemType = Helper.readInt("Enter option to select item type > ");
 
@@ -52,9 +50,7 @@ public class ResourceCentre {
             } else if (option == 3) {
                 // Loan item
                 ResourceCentre.setHeader("LOAN");
-                ResourceCentre.setHeader("ITEM TYPES");
-                System.out.println("1. Camcorder");
-                System.out.println("2. Chromebook");
+                itemTypeMenu();
 
                 int itemType = Helper.readInt("Enter option to select item type > ");
 
@@ -71,9 +67,7 @@ public class ResourceCentre {
             } else if (option == 4) {
                 // Return item
                 ResourceCentre.setHeader("RETURN");
-                ResourceCentre.setHeader("ITEM TYPES");
-                System.out.println("1. Camcorder");
-                System.out.println("2. Chromebook");
+                itemTypeMenu();
 
                 int itemType = Helper.readInt("Enter option to select item type > ");
                 if (itemType == 1) {
@@ -95,6 +89,15 @@ public class ResourceCentre {
         }
 
     }
+
+	/**
+	 * 
+	 */
+	public static void itemTypeMenu() {
+		ResourceCentre.setHeader("ITEM TYPES");
+		System.out.println("1. Camcorder");
+		System.out.println("2. Chromebook");
+	}
 
     public static void menu() {
         ResourceCentre.setHeader("RESOURCE CENTRE APP");
